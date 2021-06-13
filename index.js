21218c1d351ab382394b186a8f9bee3d7906e41b
Pierre Golfier <pgolfier.pro@gmail.com>
2021-06-13 22:17:51 +0200

    update main.yml

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ async function exec() {
   const token = core.getInput('token')
   const octokit = github.getOctokit(token)
 
+  const context = github.context
+  console.log(context)
+
   const result = await octokit.graphql(`
     {
       repository(owner: "pedraalcorp", name: "hello-world-test-action") {
@@ -19,8 +22,6 @@ async function exec() {
       }
     }
   `)
-
-
   console.log(result.repository.pullRequests.nodes)
 }
 
