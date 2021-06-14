59c62604db241af6535482c0a98d3f9199bc4904
pierre golfier <pierre@codeur.com>
2021-06-14 10:37:41 +0200

    update

--- index.js
+++ index.js
@@ -2,36 +2,41 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 async function exec() {
-  const token = core.getInput('token')
-  const octokit = github.getOctokit(token)
-  const issueKey = core.getInput('issueKey')
+  try {
+    const token = core.getInput('token')
+    const octokit = github.getOctokit(token)
+    const issueKey = core.getInput('issueKey')
 
-  const searchResult = await octokit.graphql(`
-    query targetPullRequest($queryString: String!) {
-      search(last: 1, query: $queryString, type: ISSUE) {
-        nodes {
-          ... on PullRequest {
-            title
-            headRefName
-            baseRefName
-            mergeable
-            reviewDecision
-            checksResourcePath
-            checksUrl
+    const searchResult = await octokit.graphql(`
+      query targetPullRequest($queryString: String!) {
+        search(last: 1, query: $queryString, type: ISSUE) {
+          nodes {
+            ... on PullRequest {
+              title
+              headRefName
+              baseRefName
+              mergeable
+              reviewDecision
+              checksResourcePath
+              checksUrl
+            }
           }
         }
       }
+    `,
+      {
+      queryString: `is:pr ${issueKey} in:title repo:${github.context.payload.repository.full_name}`
+      })
+    const pullRequest = searchResult.search.nodes[0]
+    if (!(pullRequest.mergeable === 'MERGEABLE' && pullRequest.reviewDecision === 'APPROVED')) {
+      throw new Error('Pull Request is not ready for merging')
     }
-  `,
-    {
-    queryString: `is:pr ${issueKey} in:title repo:${github.context.payload.repository.full_name}`
-    })
-  const pullRequest = searchResult.search.nodes[0]
-  if (!(pullRequest.mergeable === 'MERGEABLE' && pullRequest.reviewDecision === 'APPROVED')) {
-    throw new Error('Pull Request is not ready for merging')
+    core.setOutput('targetBranch', pullRequest.headRefName)
+    core.setOutput('canProceed', pullRequest.mergeable)
+  } catch (error) {
+    core.setFailed(error.toString())
   }
-  core.setOutput('targetBranch', pullRequest.headRefName)
-  core.setOutput('canProceed', pullRequest.mergeable)
+
 }
 
 exec()
