d92110c3860f6a561926a6885097a0ade4e3ad5f
pierre golfier <pierre@codeur.com>
2021-06-18 09:20:46 +0200

    update

--- index.js
+++ index.js
@@ -7,33 +7,6 @@ async function exec() {
     const octokit = github.getOctokit(token)
     const issueKey = core.getInput('issueKey')
     const mergeIn = core.getInput('mergeIn')
-
-    // console.log(issueKey, mergeIn)
-
-    // console.log(`
-    //   query targetPullRequest($queryString: String!) {
-    //     search(last: 1, query: $queryString, type: ISSUE) {
-    //       nodes {
-    //         ... on PullRequest {
-    //           title
-    //           headRefName
-    //           baseRefName
-    //           mergeable
-    //           reviewDecision
-    //           checksResourcePath
-    //           checksUrl
-    //           repository {
-    //             id
-    //           }
-    //         }
-    //       }
-    //     }
-    //   }
-    // `,
-    //   {
-    //     queryString: `is:pr ${issueKey} in:title repo:${github.context.payload.repository.full_name}`
-    //   }
-    // )
     const searchResult = await octokit.graphql(`
       query targetPullRequest($queryString: String!) {
         search(last: 1, query: $queryString, type: ISSUE) {
@@ -59,33 +32,16 @@ async function exec() {
       }
     )
     const pullRequest = searchResult.search.nodes[0]
-    // if (pullRequest.mergeable !== 'MERGEABLE' || pullRequest.reviewDecision === 'CHANGES_REQUESTED') {
-    //   console.error(`Mergeable : ${pullRequest.mergeable}, Review : ${pullRequest.reviewDecision}`)
-    //   throw new Error('Pull Request is not ready for merging')
-    // }
-
-    // console.log(`
-    //   mutation {
-    //     mergeBranch(input: { authorEmail: "pgolfier.pro@gmail.com", base: "${mergeIn}", commitMessage: "Merging ${pullRequest.headRefName} in ${mergeIn}", head: "${pullRequest.headRefName}", repositoryId: "${pullRequest.repository.id}" }) {
-    //       clientMutationId
-    //     }
-    //   }
-    // `)
-
-    const mergeMutation = await octokit.graphql(`
+    await octokit.graphql(`
       mutation {
         mergeBranch(input: { base: "${mergeIn}", commitMessage: "Merging ${pullRequest.headRefName} in ${mergeIn}", head: "${pullRequest.headRefName}", repositoryId: "${pullRequest.repository.id}" }) {
           clientMutationId
         }
       }
     `)
-    console.log(mergeMutation)
-
-    // core.setOutput('targetBranch', pullRequest.headRefName)
   } catch (error) {
     core.setFailed(error.toString())
   }
-
 }
 
 exec()
